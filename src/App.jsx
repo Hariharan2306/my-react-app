b21ec3e416a27806fd6a5c1564de03de4c3b6f28
Godwin <godwin.s@basiscloudsolutions.com>
2024-03-27 18:55:05 +0530

    added navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import { Suspense } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import LandigPage from "./landing-page/LandigPage";
+import ForumPage from "./landing-page/forumPage";
 
 function App() {
   return (
     <Router>
       <Suspense>
+        <LandigPage />
         <Routes>
-          <Route path="/landing-page" element={<LandigPage />} />
+          <Route path="/travel-forum" element={<ForumPage />} />
         </Routes>
       </Suspense>
     </Router>
